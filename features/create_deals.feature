4e5624503355ba5b8fa0fbd0b3f379446b1cb76a
saasbook <saasbook@saasbook.(none)>
2015-05-13 20:22:00 -0300

    cucumber

---
+++ features/create_deals.feature
@@ -0,0 +1,10 @@
+Feature: Create Deal
+As a user
+So that I can post an ad on the website
+I want to create a deal
+Scenario: Create a deal
+Given I am on http://localhost:3000/deals/new
+And I fill in "name" with "Moby Dick"
+Then I press "Create Deal"
+Then I am on http://localhost:3000/deals/
+Then show me the deal on the display page
---
+++ features/step_definitions/create_steps.rb
@@ -0,0 +1,27 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+
+Given(/^I am on (.+)$/) do |page_name|
+  visit page_name
+end
+
+Given(/^I fill in "(.*?)" with "(.*?)"$/) do |field, value|
+  fill_in(field, :with => value)
+end
+
+When(/^I press "(.*?)"$/) do |button|
+  click_link_or_button(button)
+end
+
+Then(/^show me the deal on the display page$/) do
+# save_and_open_page 
+puts page.body
+end
+
+
+Then(/^the page should have the message "(.*?)"$/) do |arg1|
+
+end
+
+
