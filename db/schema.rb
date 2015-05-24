3b49ef1f23b0a58b0241954c72149a1d6dcc07dd
saasbook <saasbook@saasbook.(none)>
2015-05-24 18:01:14 -0300

    final working features added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150430214353) do
+ActiveRecord::Schema.define(:version => 20150430231012) do
 
   create_table "deals", :force => true do |t|
     t.string   "category"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(:version => 20150430214353) do
     t.string   "price"
     t.string   "contactname"
     t.string   "contactemail"
-    t.string   "contactphonenumber"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.string   "contactnumber"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
   create_table "students", :force => true do |t|
